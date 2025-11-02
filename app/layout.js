b469dd8a02bf982c586c28f538551d09aa6cce1e
Abdulrazaq <abdulrazaqmyusuf@gmail.com>
2025-11-02 17:05:45 +0100

    page build progress

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,15 @@
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 
 import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: {
@@ -15,15 +23,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 min-h-screen text-primary-100">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 min-h-screen text-primary-100 flex flex-col relative`}
+      >
+        <Header />
 
-        <main>{children}</main>
-
-        <footer>Copyrights &copy; all rights reserved 2025</footer>
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
