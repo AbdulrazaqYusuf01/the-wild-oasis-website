aa8a15a12d7fbdce7695dec1f3dd071e123348b2
Abdulrazaq <abdulrazaqmyusuf@gmail.com>
2026-01-10 04:50:37 +0100

    update actions file

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,7 +3,15 @@
 import { signIn, signOut } from "./auth";
 
 export async function updateGuest(formData) {
-  console.log(formData);
+  const session = await auth;
+  if (!session) throw new Error("You must be logged in");
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid nationalID");
+
+  const guestData = { nationalID, nationality, countryFlag };
+  console.log(guestData);
 }
 
 export async function signInAction() {
